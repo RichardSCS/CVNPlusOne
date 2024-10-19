8a0ecf9d9a210f55d1d6401c12334dd5afeacd62
Rumi Ghosh <139011084+rg3374@users.noreply.github.com>
2024-10-18 20:00:41 -0700

    Update MyFileDatabaseUnitTests.cpp

--- IndividualMiniprojectC++/test/MyFileDatabaseUnitTests.cpp
+++ IndividualMiniprojectC++/test/MyFileDatabaseUnitTests.cpp
@@ -29,6 +29,6 @@ TEST_F(MyFileDatabaseUnitTests, SaveContentsToFileTest) {
 }
 
 TEST_F(MyFileDatabaseUnitTests, ToStringTest) {
-   std::string expectedResult = "For the APPT1 appointment:\n\nTitle: Doctor Appointment; Location: Clinic; Start Time: Thu, 2024-10-31 07:00; End Time: Thu, 2024-10-31 08:00\nFor the APPT2 appointment:\n\nTitle: Diagnostic Test; Location: Hospital; Start Time: Thu, 2024-10-31 07:05; End Time: Thu, 2024-10-31 08:16\nFor the APPT3 appointment:\n\nTitle: Prescription Refill; Location: Pharmacy; Start Time: Thu, 2024-10-31 07:08; End Time: Thu, 2024-10-31 08:50\nFor the APPT4 appointment:\n\nTitle: Meeting; Location: Pharmacy; Start Time: Thu, 2024-10-31 07:00; End Time: Thu, 2024-10-31 07:10\n";
+    std::string expectedResult = "For the APPT1 appointment:\n\nTitle: Doctor Appointment; Location: Clinic; Start Time: Thu, 2024-10-31 07:00; End Time: Thu, 2024-10-31 08:00\nFor the APPT2 appointment:\n\nTitle: Diagnostic Test; Location: Hospital; Start Time: Thu, 2024-10-31 07:05; End Time: Thu, 2024-10-31 08:16\nFor the APPT3 appointment:\n\nTitle: Prescription Refill; Location: Pharmacy; Start Time: Thu, 2024-10-31 07:08; End Time: Thu, 2024-10-31 08:50\n";
     ASSERT_EQ(expectedResult, testMyFileDatabase->display());
 }
