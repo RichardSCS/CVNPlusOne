cc3e23b4528cdc93eddc2e07b58893d6a59b0d35
jianyigao <jianyigao@nianticlabs.com>
2024-11-10 21:29:51 -0800

    Add create appt

--- RESTClient/PatientClient.cpp
+++ RESTClient/PatientClient.cpp
@@ -2,8 +2,9 @@
 #include "restclient-cpp/restclient.h"
 #include <json/json.h>
 #include <iostream>
+#include <regex>
 
-PatientClient::PatientClient(const std::string& baseUrl) : baseUrl(baseUrl) {}
+PatientClient::PatientClient(const std::string& baseUrl) : baseUrl(baseUrl), createdAppointments() {}
 
 std::vector<std::string> PatientClient::getAppointmentCodes() {
     RestClient::Response response = RestClient::get(baseUrl + "/listAppts");
@@ -41,8 +42,50 @@ std::string PatientClient::getAppointmentDetails(const std::string& code) {
     }
 }
 
+// Reference https://gist.github.com/arthurafarias/56fec2cd49a32f374c02d1df2b6c350f
+std::string encodeURIComponent(std::string decoded) {
+    std::ostringstream oss;
+    std::regex r("[!'\\(\\)*-.0-9A-Za-z_~]");
+
+    for (char &c : decoded) {
+        if (std::regex_match((std::string){c}, r)) {
+            oss << c;
+        } else {
+            oss << "%" << std::uppercase << std::hex << (0xff & c);
+        }
+    }
+    return oss.str();
+}
+
+std::string PatientClient::createAppointment(const std::string& title, int startTime, int endTime, const std::string& location) {
+    std::string encodedTitle = encodeURIComponent(title);
+    std::string encodedLocation = encodeURIComponent(location);
+
+    std::ostringstream urlBuilder;
+    urlBuilder << baseUrl << "/createAppt?"
+                << "title=" << encodedTitle
+                << "&startTime=" << startTime
+                << "&endTime=" << endTime
+                << "&location=" << encodedLocation;
+
+    std::cout << urlBuilder.str() << std::endl;
+    RestClient::Response response = RestClient::post(urlBuilder.str(), "application/json", "");
+
+    if (response.code == 201) {
+        std::cout << response.body << std::endl;
+        std::string apptCode = response.body.substr(response.body.find("APPT"));
+        PatientClient::createdAppointments.push_back(apptCode);
+        return apptCode;
+    } else {
+        std::cerr << "Failed to create appointment. Error: " << response.body << ". HTTP code: " << response.code << std::endl;
+        return "Failed to create appointment";
+    }
+}
+
 void PatientClient::displayAllAppointmentCodes() {
-    std::vector<std::string> codes = getAppointmentCodes();
+    // std::vector<std::string> codes = getAppointmentCodes();
+    // Only get patient client created appointments
+    std::vector<std::string> codes = PatientClient::createdAppointments;
     if (codes.empty()) {
         std::cout << "No appointment codes found\n";
     } else {
@@ -54,19 +97,31 @@ void PatientClient::displayAllAppointmentCodes() {
 }
 
 void PatientClient::displayAllAppointmentDetails() {
-    auto codes = getAppointmentCodes();
+    // auto codes = getAppointmentCodes();
+    std::vector<std::string> codes = PatientClient::createdAppointments;
     for (const auto& code : codes) {
         std::cout << "Details for " << code << ": " << getAppointmentDetails(code) << "\n\n";
     }
 }
 
 void PatientClient::displayAppointmentDetail(const std::string& code) {
-    std::cout << "Details for " << code << ": " << getAppointmentDetails(code) << "\n\n";
+    auto it = std::find(PatientClient::createdAppointments.begin(), PatientClient::createdAppointments.end(), code);
+    
+    if (it != PatientClient::createdAppointments.end()) {
+        std::cout << "Details for " << code << ": " << getAppointmentDetails(code) << "\n\n";
+    } else {
+        std::cout << "Appointment " << code << " not found\n";
+    }
 }
 
 std::string PatientClient::deleteAppointment(const std::string& code) {
-    RestClient::Response response = RestClient::get(baseUrl + "/deleteAppt?apptCode=" + code);
+    RestClient::Response response = RestClient::del(baseUrl + "/deleteAppt?apptCode=" + code);
     if (response.code == 200) {
+        auto it = std::remove(PatientClient::createdAppointments.begin(), PatientClient::createdAppointments.end(), code);
+        if (it != PatientClient::createdAppointments.end()) {
+            PatientClient::createdAppointments.erase(it, PatientClient::createdAppointments.end());
+            std::cout << "Appointment " << code << " deleted successfully\n";
+        }
         return response.body;
     } else {
         std::cerr << "Failed to delete appoitment " << code << ". HTTP code: " << response.code << "\n";
--- RESTClient/PatientClient.h
+++ RESTClient/PatientClient.h
@@ -6,6 +6,7 @@
 class PatientClient {
     private:
         std::string baseUrl;
+        std::vector<std::string> createdAppointments;
     public:
         PatientClient(const std::string& baseUrl);
         
@@ -16,6 +17,7 @@ class PatientClient {
         void displayAppointmentDetails();
         void displayAppointmentDetail(const std::string& code);
         std::string deleteAppointment(const std::string& code);
+        std::string createAppointment(const std::string& title, int startTime, int endTime, const std::string& location);
 };
 
 #endif
